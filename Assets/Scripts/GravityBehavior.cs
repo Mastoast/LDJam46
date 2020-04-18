f4dfbbeeeb48b9c0f4387cba82e0f24f264e027f
GuillaumeRusso <guillaumerusso.info@gmail.com>
2020-04-18 22:16:17 +0200

    Gravity Behavior added

---
+++ Assets/Scripts/GravityBehavior.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityBehavior : MonoBehaviour
+{
+    public float lerpSpeed = 10f;
+    public float gravity = 10f;
+    private bool isGrounded;
+    public float deltaGround = 0.2f;
+
+    private Vector3 surfaceNormal;
+    private Vector3 myNormal;
+    private float distGround;
+
+    private Transform myTransform;
+    public CapsuleCollider capsuleCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myNormal = transform.up;
+        myTransform = transform;
+        GetComponent<Rigidbody>().freezeRotation = true;
+
+        distGround = capsuleCollider.bounds.extents.y - capsuleCollider.center.y;
+    }
+
+    private void FixedUpdate()
+    {
+        GetComponent<Rigidbody>().AddForce(-gravity * GetComponent<Rigidbody>().mass * myNormal);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray;
+        RaycastHit hit;
+
+
+        ray = new Ray(myTransform.position, -myNormal);
+        if (Physics.Raycast(ray, out hit))
+        {
+            isGrounded = hit.distance <= distGround + deltaGround;
+            surfaceNormal = hit.normal;
+        }
+        else
+        {
+            isGrounded = false;
+            surfaceNormal = Vector3.up;
+        }
+
+        myNormal = Vector3.Lerp(myNormal, surfaceNormal, lerpSpeed * Time.deltaTime);
+
+        Vector3 myForward = Vector3.Cross(myTransform.right, myNormal);
+
+        Quaternion targetRot = Quaternion.LookRotation(myForward, myNormal);
+        myTransform.rotation = Quaternion.Lerp(myTransform.rotation, targetRot, lerpSpeed * Time.deltaTime);
+
+    }
+}
