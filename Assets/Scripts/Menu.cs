e251f70c30e2039dc2383782977b207021d7da13
GuillaumeRusso <guillaumerusso.info@gmail.com>
2020-04-19 00:58:45 +0200

    Menu Added

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    //Menu Manager
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    //Menu Pause
+    public void ResumeGame(GameObject PauseMenu)
+    {
+        PauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+}
