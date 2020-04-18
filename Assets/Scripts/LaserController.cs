e79b929a9d2624976398fae935d9a58fed8a4c6a
Mastoast <dgekec@gmail.com>
2020-04-18 18:03:20 +0200

    Add Laser prefab

---
+++ Assets/Scripts/LaserController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserController : MonoBehaviour
+{
+    public float speed = 10.0f;
+
+    private Rigidbody rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        rigidbody.velocity = new Vector3(0, 0, 10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
