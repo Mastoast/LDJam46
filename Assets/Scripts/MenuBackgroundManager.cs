18343859a24a29b309db7561ec007ab00231a594
GuillaumeRusso <guillaumerusso.info@gmail.com>
2020-04-19 12:57:35 +0200

    Main Menu Update

---
+++ Assets/Scripts/MenuBackgroundManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuBackgroundManager : MonoBehaviour
+{
+    public GameObject Starship;
+    public float Speed = 20f;
+
+    private Vector3 starshipMovement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        starshipMovement = new Vector3(0f, 0f, -Speed);
+    }
+
+    void FixedUpdate()
+    {
+        Starship.transform.localPosition = (Starship.transform.localPosition + starshipMovement);
+    }
+}
