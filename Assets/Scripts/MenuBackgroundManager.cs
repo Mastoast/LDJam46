2aa98dfd722356038e5955d50d1ff9495b4c1425
GuillaumeRusso <guillaumerusso.info@gmail.com>
2020-04-20 20:04:33 +0200

    Added Respawn and Stock + Menu bidule Version

--- Assets/Scripts/MenuBackgroundManager.cs
+++ Assets/Scripts/MenuBackgroundManager.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class MenuBackgroundManager : MonoBehaviour
 {
-    public GameObject Starship;
-    public float Speed = 20f;
+    public GameObject starship;
+    public GameObject bidule;
+
+    public float starshipMovementSpeed = 0.3f;
+    public float biduleMovementSpeed = 0.05f;
+    public float biduleRotationSpeed = 1f;
+
 
     private Vector3 starshipMovement;
+    private Vector3 biduleMovement;
+    private Vector3 biduleRotation;
+
 
 
     void FixedUpdate()
     {
-        starshipMovement = Starship.transform.forward * Speed;
-        Starship.transform.localPosition = (Starship.transform.localPosition + starshipMovement);
+
+        biduleMovement = bidule.transform.right * -biduleMovementSpeed;
+        bidule.transform.localPosition = (bidule.transform.localPosition + biduleMovement);
+
+        starshipMovement = starship.transform.forward * starshipMovementSpeed;
+        starship.transform.localPosition = (starship.transform.localPosition + starshipMovement);
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 15f;
+    public float maxDistanceFromShip = 200f;
+    public GameObject attachedShip;
+    public GameObject RespawnPoint;
 
     private Rigidbody rb;
     private Vector3 moveDir;
@@ -20,11 +23,21 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+
+        if(Vector3.Distance(this.transform.position, attachedShip.transform.position) >= maxDistanceFromShip)
+        {
+            Death();
+        }
     }
 
     void FixedUpdate()
     {
         if (canMove)
             transform.position += transform.TransformDirection(moveDir) * speed * Time.fixedDeltaTime;
+    }
+
+    public void Death()
+    {
+        this.transform.position = RespawnPoint.transform.position;
     }
 }
