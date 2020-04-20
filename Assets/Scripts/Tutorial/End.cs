2bc6d371c6a5704873f6466fc8cd65130691988f
troncoli <thomas.roncoli@gmail.com>
2020-04-20 21:53:39 +0200

    Finished tutorial structure

---
+++ Assets/Scripts/Tutorial/End.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class End : Tutorial
+{
+    public GameObject entireMesh;
+    public GameObject armMesh;
+    public Camera cameraPlayer;
+    public Camera cameraRobot;
+    public PlayerMovement pm;
+    public Canvas canvas;
+    private float startTime;
+    private bool justStarted = true;
+
+        
+    public override void checkIfHappening()
+    {
+        if (justStarted)
+        {
+            cameraPlayer.enabled = false;
+            cameraRobot.enabled = true;
+            
+            canvas.enabled = false;
+            pm.enabled = false;
+            armMesh.SetActive(false);
+            entireMesh.SetActive(true);
+            
+            startTime = Time.time;
+            justStarted = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Tutorial/Refill.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Refill : Tutorial
+{
+    public Stock stock;
+    public override void checkIfHappening()
+    {
+        if (stock.bonbonne > 0)
+        {
+            TutorialManager.Instance.completedTutorial();
+        }
+    }
+}
